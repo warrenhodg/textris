5f58e16e6cc0534676130f4e8546dc066b02d227
Warren Hodgkinson <warren.hodgkinson@jet.com>
2020-04-23 08:47:01 +0100

    Added comparison and display traits to Colour

--- src/tris/colour.rs
+++ src/tris/colour.rs
@@ -1,5 +1,16 @@
-#[derive(Copy, Clone)]
+use std::fmt;
+
+#[derive(Copy, Clone, PartialEq)]
 pub enum Colour {
     Empty,
     Value(u16),
+}
+
+impl fmt::Display for Colour {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Colour::Empty => write!(f, "empty"),
+            Colour::Value(v) => write!(f, "#{0}", v),
+        }
+    }
 }
