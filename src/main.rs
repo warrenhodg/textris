621b4d63206afb33e9349c23004e16445885cb6e
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2020-04-23 09:53:02 +0100

    Added version
    Co-authored-by: Warren Hodgkinson <warren.hodgkinson@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 pub mod tris;
 
-use tris::Block;
-use tris::VecGame;
 use tris::Game;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() -> () {
-    let mut b = tris::UBlock::new(tris::BlockType::T);
+    let b = tris::UBlock::new(tris::BlockType::T);
 
     let mut g = tris::VecGame::new(10, 10)
         .ok()
         .expect("invalid board size");
 
     g.merge(&b, 1, 2);
+
+    println!("textris-{}", VERSION);
 }
