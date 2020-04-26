b11f5097d3f773b796625d5593edaab635a04ba9
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2020-04-26 10:31:23 +0100

    Feat/async input (#5)
    Async input

--- src/gameloop/mod.rs
+++ src/gameloop/mod.rs
@@ -1,7 +1,8 @@
 use super::input::Input;
 use super::output::Output;
 use super::tris::VecGame;
-use termion::event::Key;
+
+const GAME_LOOP_PERIOD: std::time::Duration = std::time::Duration::from_millis(10);
 
 pub struct GameLoop<'a> {
     input: &'a mut dyn Input,
@@ -17,17 +18,30 @@ pub fn new<'a>(input: &'a mut dyn Input, output: &'a mut dyn Output) -> GameLoop
 
 impl <'a> GameLoop<'a> {
     pub fn run(&mut self) {
+        let mut changed = true;
+        let mut message = format!("");
+
         'run_loop: loop {
-            self.output.show_main_menu();
+            if changed {
+                self.output.show_main_menu();
+                self.output.show_message(message.to_string());
+                changed = false;
+            }
 
             match self.input.get_key() {
-                None => (),
-                Some(k) => {
-                    match k.unwrap() {
-                        Key::Char('q') => break 'run_loop,
-                        Key::Char('n') => self.play_game(),
-                        _ => self.message(format!("{}Unknown command\r\n", termion::clear::CurrentLine).to_string()),
-                    }
+                None => {
+                    std::thread::sleep(GAME_LOOP_PERIOD);
+                },
+                Some(k) => match k {
+                    'q' => break 'run_loop,
+                    'n' => {
+                        self.play_game();
+                        changed = true;
+                    },
+                    _ => {
+                        message = format!("{}Unknown command {}\r\n", termion::clear::CurrentLine, k as u32);
+                        changed = true;
+                    },
                 }
             }
         }
@@ -36,15 +50,26 @@ impl <'a> GameLoop<'a> {
     }
 
     fn play_game(&mut self) {
+        let mut changed = true;
         let g = &mut VecGame::new(10, 10).unwrap();
 
-        self.output.show_message("playing".to_string());
         self.output.show_game(g);
-        std::thread::sleep(std::time::Duration::from_millis(2000));
-    }
 
-    fn message(&mut self, message: String) {
-        self.output.show_message(message);
-        std::thread::sleep(std::time::Duration::from_millis(2000));
+        'play_loop: loop {
+            if changed {
+                self.output.show_game(g);
+                changed = false;
+            }
+
+            match self.input.get_key() {
+                None => {
+                    std::thread::sleep(GAME_LOOP_PERIOD);
+                },
+                Some(k) => match k {
+                    'q' => break 'play_loop,
+                    _ => (),
+                }
+            }
+        }
     }
 }
