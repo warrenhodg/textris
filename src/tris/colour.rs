9f4d2f44c94e64a3cf6e853d7d3d4a90582f871e
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2020-04-26 15:44:31 +0100

    Add colour displaying

--- src/tris/colour.rs
+++ src/tris/colour.rs
@@ -3,7 +3,16 @@ use std::fmt;
 #[derive(Copy, Clone, PartialEq)]
 pub enum Colour {
     Empty,
-    Value(u16),
+    Value(usize),
+}
+
+impl fmt::Debug for Colour {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Colour::Empty => write!(f, "empty"),
+            Colour::Value(v) => write!(f, "#{0}", v),
+        }
+    }
 }
 
 impl fmt::Display for Colour {
