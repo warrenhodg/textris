a344638f932d48fbb774de1869e7956cfc458b14
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2020-04-26 15:45:53 +0100

    Expose TEST_BLOCK for test configs

--- src/tris/mod.rs
+++ src/tris/mod.rs
@@ -1,8 +1,9 @@
 pub mod block;
 
-pub use self::block::BlockType;
 pub use self::block::Block;
 pub use self::block::UBlock;
+#[cfg(test)]
+pub use self::block::TEST_BLOCK;
 
 pub mod colour;
 pub use self::colour::Colour;
